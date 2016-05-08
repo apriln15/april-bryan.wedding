fc7cf36e0f84becd69d86952bf0252ee7180b47b
Bryan Mikaelian <bryan@librato.com>
2016-05-08 12:39:11 -0500

    More UI progress

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,3 +1,29 @@
 //= require ./parallax
 
 window.Wedding = {};
+
+(function() {
+  var waypointNav;
+
+  Wedding.init = function() {
+    new Waypoint({
+      element: document.getElementById('stories'),
+      handler: function(direction) {
+        if (direction === 'down') {
+          $('nav').addClass('visible').removeClass('hiding');
+        }
+      },
+    });
+
+    new Waypoint({
+      element: document.getElementById('stories'),
+      handler: function(direction) {
+        if (direction === 'up') {
+          $('nav').addClass('hiding').removeClass('visible');
+        }
+      },
+    });
+  }
+})()
+
+Wedding.init();
